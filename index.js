097c89e3efa3917124e3937d9322257375884224
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-06-16 17:19:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 // Challenge1
 function saturdayFun(activity="roller-skate") {
-  console.log(`This Saturday, I want to ${activity}!`);
+  return `This Saturday, I want to ${activity}!`;
 }
 
 const mondayWork = function (activity="go to the office") {
-  console.log(`This Monday, I will ${activity}!`);
+  return `This Monday, I will ${activity}!`;
 }
 mondayWork()
 
