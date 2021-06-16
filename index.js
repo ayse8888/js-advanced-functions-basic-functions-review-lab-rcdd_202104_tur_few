4a7ee181e6dbef221cf2df098d91157aa3f044b5
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-06-16 17:18:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+// Challenge1
+function saturdayFun(activity="roller-skate") {
+  console.log(`This Saturday, I want to ${activity}!`);
+}
+
+const mondayWork = function (activity="go to the office") {
+  console.log(`This Monday, I will ${activity}!`);
+}
+mondayWork()
+
+
+// Challenge3
+function wrapAdjective(str="*") {
+  return function(adjective="special") {
+    return `You are ${str}${adjective};`
+  }
+}
+
+wrapAdjective("||")("a dedicated programmer")
+wrapAdjective()("a hard worker")
+
+
+// Challenge4
+const Calculator = {}
+function add(){
+  return Calculator.add = 1 + 3
+}
+add()
+
+
+
